2e8a291679764915f915c3a307b89a32f0d349cd
Vaibhav Shukla <54389264+vaibhav30062002@users.noreply.github.com>
2019-10-28 01:13:26 +0530

    Create sum_using_while.c

---
+++ sum_using_while.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+        int sum=0,i=1;
+        while(i<101)
+        {
+                sum =sum+i;
+                i++;
+        }
+        printf("sum of numbers from 0 to 100 is %d\n",sum);
+        return 0;
+}
+**************************************************************
+OUTPUT:
+sum of numbers from 0 to 100 is 5050
+        
