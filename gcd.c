ef4f9a082e4964e4875fcff3cfdec47e535b61e5
Vaibhav Shukla <vaibhav2017shukla@gmail.com>
2019-10-30 00:03:28 +0530

    Create gcd.c

---
+++ gcd.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main()
+{
+int m,n,r=1;
+printf("\n Enter value for m,n\n");
+scanf("%d%d",&m,&n);
+while(r!=0)
+{
+r=n%m;
+n=m;
+m=r;
+}
+printf("\n GCD=%d \n",n);
+return 0;
+}
+
+/**********************************************
+
+OUTPUT:
+Enter value for m,n
+36
+24
+
+ GCD=12 
+ 
+ *************************************************/
