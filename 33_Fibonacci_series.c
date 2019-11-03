7e912632f6dcaf93af29625d94b210b947f83157
Vaibhav Shukla <vaibhav2017shukla@gmail.com>
2019-11-03 11:34:43 +0530

    Update 33_Fibonacci_series.c

--- 33_Fibonacci_series.c
+++ 33_Fibonacci_series.c
@@ -1,16 +1,15 @@
 #include<stdio.h>
 void main()
 {
-        int a=1,b=1,c=0;
+        int a=0,b=1,c=0;
         int n;
-        printf("enter the number upto which you want to print series\n");
+        printf("enter the number  of terms upto which you want to print series\n");
         scanf("%d",&n);
-        printf("1\t1\t");
-        while(c<=n)
+        printf("0\t1\t");
+        for(int i=1;i<=n-2;i++)
         {
                 c=0;
                 c=a+b;
-                if(c<=n)
                 printf("%d\t",c);
         
                 a=b;
@@ -23,8 +22,7 @@ void main()
 /**************************************************************************************
 
 OUTPUT:
-enter the number upto which you want to print series
-35
-1       1       2       3       5       8       13      21      34
-
+enter the number  of terms upto which you want to print series                                                                                
+10                                                                                                                                            
+0       1       1       2       3       5       8       13      21      34    
 ***************************************************************************************/
