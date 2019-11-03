11b5a7c4b9b9af75e344c3e6bead726f3f443ecb
Vaibhav Shukla <vaibhav2017shukla@gmail.com>
2019-11-03 10:48:14 +0530

    Create 36_transpose_matrix.c

---
+++ 36_transpose_matrix.c
@@ -0,0 +1,56 @@
+#include<stdio.h>
+int main()
+{ 
+        int m,n,i,j;
+        printf("enter the number of rows and columns of matrix\n");
+        scanf("%d%d",&m,&n);
+        int a[m][n],b[m][n] ;
+                printf("enter the elements of  matrix row wise\n");
+                for(i=0;i<m;i++)
+                {
+                        for(j=0;j<n;j++)
+                        {
+                                scanf("%d",&a[i][j]);
+                        }       
+                }       
+                
+                for(i=0;i<m;i++)
+                {
+                        for(j=0;j<n;j++)
+                        {
+                                b[j][i]=a[i][j];
+                        }       
+                }       
+                printf("transpose  of matrix is \n");
+                for(i=0;i<m;i++)
+                {
+                        for(j=0;j<n;j++)
+                        {
+                                printf("%d ",b[i][j]);
+                        }       
+                        printf("\n");
+                }       
+}               
+
+/***********************************************************************************************
+
+OUTPUT:
+enter the number of rows and columns of matrix
+3
+3
+enter the elements of  matrix row wise
+1
+6
+4
+5
+3
+2
+8
+9
+7
+transpose  of matrix is 
+1 5 8 
+6 3 9 
+4 2 7 
+
+*****************************************************************************************************/
